2541922ee71a8a62374732640acff8fd05bcd059
dawn-fall <lpmcnabb@outlook.com>
2023-08-24 13:39:28 +1200

    style changes, changed word changes/removals

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,9 @@
 export default function Home()
 {
     return(<><div className="home component container2 margin cent">
+            <br/>
             <h1>Welcome to the SCP Foundation</h1>
-            <p>This is the <strong>[EXPERIMENTAL JSON]</strong> web-based catalogue system.</p>
+            <p>This is the <strong>[EXPERIMENTAL JSON & REACT]</strong> web-based catalogue system.</p>
             <br />
             <h2><strong>Reminder:</strong></h2>
             <p>This is classified infomation, and you are not permitted to share this with anybody outside of the Foundation.</p> 
--- src/Subjects.js
+++ src/Subjects.js
@@ -1,13 +1,13 @@
 export default function Subjects({subjects}){
     return(
         <div className="component container2 margin cent">
-            <h1>Subject: {subjects.Subject}</h1>
+            <h1>{subjects.Subject}</h1>
             <p><img src={subjects.Image}></img></p>
             <h2>Class: {subjects.Class}</h2>
             <br/>
             <h3>Containment infomation:</h3>
             <p>{subjects.Containment}</p>
-            <h3>Summary:</h3>
+            <h3>Description:</h3>
             <p>{subjects.Summary}</p>
         </div>
     )
